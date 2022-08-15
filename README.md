c0c1951ef5f1841b5298565a90530e96bd638829
Zack Stevens <zack.st7@gmail.com>
2022-08-15 14:56:37 -0600

    fix: Initial implementation (#1)

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # envoy-authorization-utilities
+
+This is a library designed to work with the Envoy external authorization [GRPC protocol](https://www.envoyproxy.io/docs/envoy/latest/api-v3/service/auth/v3/external_auth.proto).
+The intent is to allow configurable auth exceptions based on either path, or graphql operation in the request body. Instantiate the utility and then pass it a request object, then do whatever auth you need to based on the result.
+
+## Supported mechanisms
+* URL Path
+  * Ignore requests that match a list of url paths, including wildcards 
+  * Implemented using [URL Path](https://github.com/ucarion/urlpath)
+* Graphql Query
+  * Ignore requests that exactly match a list of graphql operation names
+
+## Usage
+Instantiate a new struct with your settings, typically by marshalling json to allow configuration via env var or some other mechanism, but you can also use the `NewAuthorizationUtils` constructor and pass a map of host settings.
+```go
+hosts := map[string]pkg.HostSettings{
+  "myhost.com": {
+    IgnorePaths: []string{"/some/path/*"},
+    IgnoreGraphqlOperations: []string["yourQueryName"],
+  },
+}
+authUtils, err := pkg.NewAuthorizationUtils(hosts)
+```
+Then pass the v3 request to `ShouldIgnoreRequest`
+```go
+shouldIgnore := authUtils.ShouldIgnoreRequest(req)
+if shouldIgnore {
+    // return a v3 check response indicating the request is authenticated
+    return &v3.CheckResponse{
+        Status: &status.Status{Code: int32(codes.OK)},
+        HttpResponse: &v3.CheckResponse_OkResponse{
+            OkResponse: &v3.OkHttpResponse{},
+        },
+    }
+}
+if !shouldIgnore {
+    // your auth logic here
+}
+```
+
+## Benchmarks
+```shell
+> go test -bench=.
+goos: linux
+goarch: amd64
+pkg: github.com/catalystsquad/envoy-authorization-utilities/test
+cpu: AMD Ryzen 9 5950X 16-Core Processor            
+BenchmarkShouldAuthUrlPathMatch-32             	14814903	        90.45 ns/op
+BenchmarkShouldAuthGraphqlOperationMatch-32    	 1669678	       754.0 ns/op
+PASS
+ok  	github.com/catalystsquad/envoy-authorization-utilities/test	3.420s
+```
---
+++ pkg/auth.go
@@ -0,0 +1,143 @@
+package pkg
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/emirpasic/gods/sets/hashset"
+	v3 "github.com/envoyproxy/go-control-plane/envoy/service/auth/v3"
+	"github.com/tidwall/gjson"
+	"github.com/ucarion/urlpath"
+	"strings"
+)
+
+type AuthorizationUtils struct {
+	Hosts map[string]HostSettings `json:"hostSettings"`
+}
+
+type HostSettings struct {
+	IgnorePaths                []string       `json:"ignorePaths"`
+	IgnoreUrlPaths             []urlpath.Path `json:"-"`
+	IgnoreGraphqlOperations    []string       `json:"ignoreGraphqlOperations"`
+	IgnoreGraphqlOperationsSet *hashset.Set   `json:"-"`
+}
+
+func NewAuthorizationUtils(hosts map[string]HostSettings) (*AuthorizationUtils, error) {
+	var authUtils AuthorizationUtils
+	hostsJson, err := json.Marshal(hosts)
+	if err != nil {
+		return nil, err
+	}
+	authUtilsJson := []byte(fmt.Sprintf(`{"hostSettings": %s}`, string(hostsJson)))
+	err = json.Unmarshal(authUtilsJson, &authUtils)
+	return &authUtils, err
+}
+
+func (a *AuthorizationUtils) ShouldIgnoreRequest(request *v3.CheckRequest) bool {
+	requestHost := request.Attributes.Request.Http.Host
+	settings, ok := a.Hosts[requestHost]
+	if !ok {
+		// no exceptions configured for this host, return false
+		return false
+	}
+	// request host matches ignore configurations, check the request
+	return settings.shouldIgnoreRequest(request)
+}
+
+func (h *HostSettings) shouldIgnoreRequest(request *v3.CheckRequest) bool {
+	requestPath := getPathFromRequest(request)
+	// check paths first
+	for _, path := range h.IgnoreUrlPaths {
+		_, match := path.Match(requestPath)
+		if match {
+			// path matches an ignored path, return true
+			return true
+		}
+	}
+	// path is not ignored, check graphql operations
+	body := getBodyFromRequest(request)
+	operationName := getGraphqlOperationFromBody(body)
+	if operationName == "" {
+		// not a graphql request so can't match against graphql queries, return false
+		return false
+	}
+	// if the host's configuration contains the operation, return true to ignore the request
+	return h.IgnoreGraphqlOperationsSet.Contains(operationName)
+}
+
+func getGraphqlOperationFromBody(body string) string {
+	// get the query
+	query := gjson.Get(body, "query").String()
+	if query == "" {
+		return ""
+	}
+	// strip newlines
+	query = removeNewlines(query)
+	firstBracket := strings.Index(query, "{")
+	if firstBracket == -1 {
+		return ""
+	}
+	secondBracket := strings.Index(query[firstBracket+1:], "{")
+	if secondBracket == -1 {
+		return ""
+	}
+	from := firstBracket + 1
+	to := firstBracket + secondBracket
+	segment := query[from:to]
+	firstParen := strings.Index(segment, "(")
+	if firstParen > -1 {
+		segment = segment[0:firstParen]
+	}
+	return removeSpaces(segment)
+}
+
+func removeNewlines(theString string) string {
+	return strings.Replace(theString, "\n", "", -1)
+}
+
+func removeSpaces(theString string) string {
+	return strings.Replace(theString, " ", "", -1)
+}
+
+func getPathFromRequest(request *v3.CheckRequest) string {
+	return request.Attributes.Request.Http.Path
+}
+
+func getBodyFromRequest(request *v3.CheckRequest) string {
+	// attempt to return body
+	body := request.Attributes.Request.Http.Body
+	if body != "" {
+		return body
+	}
+
+	// attempt to return raw body a string
+	rawBody := request.Attributes.Request.Http.RawBody
+	if len(rawBody) > 0 {
+		return string(rawBody)
+	}
+
+	// neither are present, return empty string
+	return ""
+}
+
+func (h *HostSettings) UnmarshalJSON(data []byte) error {
+	type Alias HostSettings
+	aux := &struct {
+		*Alias
+	}{
+		Alias: (*Alias)(h),
+	}
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+	if len(h.IgnorePaths) > 0 {
+		h.IgnoreUrlPaths = []urlpath.Path{}
+		for _, pathString := range h.IgnorePaths {
+			h.IgnoreUrlPaths = append(h.IgnoreUrlPaths, urlpath.New(pathString))
+		}
+	}
+	h.IgnoreGraphqlOperationsSet = hashset.New()
+	for _, operation := range h.IgnoreGraphqlOperations {
+		h.IgnoreGraphqlOperationsSet.Add(operation)
+	}
+	return nil
+}
